c89bcace7cf636b4bbf90f3c174f2183646dcc08
Francis <franciskarani15@gmail.com>
2025-06-09 21:35:12 +0300

    works on javascript arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; 
 
-// Write your code here
+
+const random = Math.ceil(Math.random() * 100); 
+
+
+
+const num3 = 10; 
+const num4 = 6;  
+const mod = num3 % num4; 
+
+
+
+const max = Math.max(5, 12, 8, 20, 15); 
+
+
+
+console.log("multiply:", multiply); 
+console.log("random:", random);     
+console.log("mod:", mod);          
+console.log("max:", max);    
